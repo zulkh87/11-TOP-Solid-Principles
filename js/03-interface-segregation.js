72b3f9885a9a7caae3e0ac1df129f55a1184e8f9
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-03 19:55:39 +0300

    interface segregation applied

--- js/03-interface-segregation.js
+++ js/03-interface-segregation.js
@@ -1,57 +1,62 @@
 class Entity {
-  constructor(name, attackDamage, health) {
+  constructor(name) {
     this.name = name;
-    this.attackDamage = attackDamage;
-    this.health = health;
   }
+}
 
+const mover = {
   move() {
     console.log(`${this.name} moved`);
-  }
+  },
+};
 
+const attacker = {
   attack(targetEntity) {
     console.log(
       `${this.name} attacked ${targetEntity.name} for ${this.attackDamage} damage`
     );
     targetEntity.takeDamage(this.attackDamage);
-  }
+  },
+};
 
+const hasHealth = {
   takeDamage(amount) {
     this.health -= amount;
     console.log(`${this.name} has ${this.health} health remaining`);
+  },
+};
+
+class Character extends Entity {
+  constructor(name, attackDamage, health) {
+    super(name);
+    this.attackDamage = attackDamage;
+    this.health = health;
   }
 }
 
-class Character extends Entity {}
+Object.assign(Character.prototype, mover);
+Object.assign(Character.prototype, attacker);
+Object.assign(Character.prototype, hasHealth);
 
 class Wall extends Entity {
   constructor(name, health) {
     super(name, attackDamage, -1);
   }
-
-  move() {
-    return null;
-  }
-
-  attack() {
-    return null;
-  }
 }
+
+Object.assign(Wall.prototype, hasHealth);
 
 class Turret extends Entity {
   constructor(name, attackDamage) {
-    super(name, attackDamage, -1);
-  }
-
-  move() {
-    return null;
-  }
-
-  takeDamage() {
-    return null;
+    super(name);
+    this.attackDamage = attackDamage;
   }
 }
 
 const turret = new Turret("Turret", 5);
 const character = new Character("Character", 3, 5);
 const wall = new Wall("Wall", 200);
+
+turret.attack(characer);
+character.move();
+character.attack(wall);
