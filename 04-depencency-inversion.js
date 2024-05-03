90bb2f67c128093720b8f42c225208aae145259f
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-04 01:14:37 +0300

    dependency inversion applied

--- 04-depencency-inversion.js
+++ 04-depencency-inversion.js
@@ -1,18 +1,26 @@
 class Store {
-  constructor(user) {
-    this.paypal = new Paypal();
-    this.user = user;
-    // this.stripe = new Stripe(user);
+  constructor(paymentProcessor) {
+    this.paymentProcessor = paymentProcessor;
   }
 
   purchaseBike(quantity) {
-    this.paypal.makePayment(this.user, 200 * quantity);
+    this.paymentProcessor.pay(200 * quantity);
     // this.stripe.makePayment(200 * quantity * 100);
   }
 
   purchaseHelmet(quantity) {
-    this.paypal.makePayment(this.user, 15 * quantity);
+    this.paymentProcessor.pay(15 * quantity);
     // this.stripe.makePayment(15 * quantity * 100);
+  }
+}
+
+class StripePaymentProcessor {
+  constructor(user) {
+    this.stripe = new Stripe(user);
+  }
+
+  pay(amountInGil) {
+    this.stripe.makePayment(amountInGil * 100);
   }
 }
 
@@ -28,12 +36,29 @@ class Stripe {
   }
 }
 
+class PaypalPaymentProcessor {
+  constructor(user) {
+    this.user = user;
+    this.paypal = new Paypal();
+  }
+
+  pay(amountInGil) {
+    this.paypal.makePayment(this.user, amountInGil);
+  }
+}
+
 class Paypal {
   makePayment(user, amountInGil) {
     console.log(`${user} made payment of Gil${amountInGil} with Paypal`);
   }
 }
 
-const store = new Store("Blue");
+const store = new Store(new StripePaymentProcessor("Blue"));
 store.purchaseBike(2);
 store.purchaseHelmet(2);
+
+const store2 = new Store(new PaypalPaymentProcessor("Blue"));
+store.purchaseBike(2);
+store.purchaseHelmet(2);
+
+// Any higher classes should always depend upon the abstraction of the class rather than the detail
